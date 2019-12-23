24ca29f8115e6641b26333b5efa6dc1ba46c75d3
Thomas Anchor <thomas.anchor98@gmail.com>
2019-12-23 00:57:16 -0500

    Create controllers

---
+++ API/Controllers/CheckInController.cs
@@ -0,0 +1,38 @@
+﻿using AspNet.Security.OpenIdConnect.Extensions;
+using AspNet.Security.OpenIdConnect.Primitives;
+using AspNet.Security.OpenIdConnect.Server;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Validation;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenIddict.Abstractions;
+using OpenIddict.Core;
+using OpenIddict.EntityFrameworkCore.Models;
+using API.Models;
+using API.Data;
+
+namespace API.Controllers
+{
+    [Authorize(AuthenticationSchemes = OpenIddictValidationDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/check-in/[action]")]
+    public class CheckInController : ControllerBase
+    {
+        private readonly OpenIddictApplicationManager<OpenIddictApplication> applicationManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CheckInController(
+            OpenIddictApplicationManager<OpenIddictApplication> applicationManager,
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.applicationManager = applicationManager;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+    }
+}
