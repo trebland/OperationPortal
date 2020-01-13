2b50e866ff2837b4725f7d18630a0fe06e33dc1b
Thomas Anchor <thomas.anchor98@gmail.com>
2020-01-12 19:04:12 -0500

    Lay out calendar API endpoints

--- API/Data/SampleRepository.cs
+++ API/Data/SampleRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Options;
 using Npgsql;
 using API.Models;
 
