3081c01753c075e80a58f42a94abecfa723bc2b0
Thomas Anchor <thomas.anchor98@gmail.com>
2020-01-22 15:52:38 -0500

    Create endpoints for individual signup and cancellations

---
+++ API/Models/AttendanceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class AttendanceModel
+    {
+        public int Id { get; set; }
+        public int VolunteerId { get; set; }
+        public DateTime DayAttended { get; set; }
+        public bool Scheduled { get; set; }
+        public bool Attended { get; set; }
+    }
+
+    // A helper class used to be able to pull a date in from a JSON request more easily due to quirks of MVC's model binding
+    public class DateModel
+    {
+        public DateTime Date { get; set; }
+    }
+}
