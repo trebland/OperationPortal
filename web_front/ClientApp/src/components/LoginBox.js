2e1ce9c0438121b94b5263089d2425ae59592cf5
baldwinsuen <suenbaldwin@gmail.com>
2020-02-15 02:15:46 -0500

    loading animation and user specific views

--- web_front/ClientApp/src/components/LoginBox.js
+++ web_front/ClientApp/src/components/LoginBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Form, FormControl, FormGroup, FormLabel, Button } from 'react-bootstrap/'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Router } from 'react-router-dom'
 
 export class LoginBox extends Component {
 
@@ -11,7 +11,8 @@ export class LoginBox extends Component {
       password: "",
       result: "",
       redirect: false,
-      jwt: ""
+      jwt: "",
+      loggedin: false
     };
     this.handleUserNameChange = this.handleUserNameChange.bind(this)
     this.handlePasswordChange = this.handlePasswordChange.bind(this)
@@ -31,57 +32,61 @@ export class LoginBox extends Component {
     console.log(this.state.password)
   }
 
-  postAndFetchData = (path) => {
-    fetch('http://operation-portal.com' + path , {
-      method: "POST",
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(this.state)
-      })
-      .then((response) => {
-        console.log(response.status)
-        if(response.status === 200 || response.status === 201) {
-          return response.text()
-        } else if (response.status === 401 || response.status === 400 || response.status === 500 && this.mounted === true) {
-          this.setState({
-            redirect: false,
-            result: 'Username or password incorrect.'
-          })
-          return
-        }
-      })
-      .then((data) => {
-        let res = JSON.parse(data)
-        res = res.token
-        if(this.mounted == true) {
-          this.setState({
-            jwt: res
-          })
-        }
-        console.log(this.state.jwt)
-      })
-      .then(() => {this.setRedirect()})
-      .catch(() => {
-        console.log('didnt post')
-      })
-  }
+  // postAndFetchData = (path) => {
+  //   fetch('http://operation-portal.com' + path , {
+  //     method: "POST",
+  //     headers: {
+  //       'Content-type': 'application/json'
+  //     },
+  //     body: JSON.stringify(this.state)
+  //     })
+  //     .then((response) => {
+  //       console.log(response.status)
+  //       if(response.status === 200 || response.status === 201) {
+  //         return response.text()
+  //       } else if (response.status === 401 || response.status === 400 || response.status === 500 && this.mounted === true) {
+  //         this.setState({
+  //           redirect: false,
+  //           result: 'Username or password incorrect.'
+  //         })
+  //         return
+  //       }
+  //     })
+  //     .then((data) => {
+  //       let res = JSON.parse(data)
+  //       res = res.token
+  //       if(this.mounted == true) {
+  //         this.setState({
+  //           jwt: res
+  //         })
+  //       }
+  //       console.log(this.state.jwt)
+  //     })
+  //     .then(() => {this.setRedirect()})
+  //     .catch(() => {
+  //       console.log('didnt post')
+  //     })
+  // }
 
-  onSubmit = (e) => {
-    this.postAndFetchData('api/auth/token')
-  }
+  // onSubmit = (e) => {
+  //   this.postAndFetchData('api/auth/token')
+  // }
 
   setRedirect = () => {
     this.setState({
-      redirect: true
+      redirect: true,
+      loggedin: true
     })
   }
 
   renderRedirect = () => {
     if (this.state.redirect) {
       return <Redirect to={{
-        pathname: '/',
-        state: { jwt: this.state.jwt }
+        pathname: '/testing',
+        state: { 
+          username: this.state.username,
+          loggedin: true
+        }
       }}/>
     }
   }
@@ -115,10 +120,9 @@ export class LoginBox extends Component {
             <p>{this.state.result}</p>
             <div>
               {this.renderRedirect()}
-              <Button type="submit" size="lg" onClick={this.onSubmit} >
+              <Button type="submit" size="lg" onClick={this.setRedirect} >
                 Submit
               </Button>
-              <p>{this.state.result}</p>
             </div>
           </Form>
         </div>
