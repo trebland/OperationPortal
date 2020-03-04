91173dde25f30418757a69be8f55caa670ed6417
Trevor B <tbland96@knights.ucf.edu>
2020-03-03 21:33:36 -0500

    Update Staff_ProfileViewer.dart

--- basic_front/lib/Staff/Staff_ProfileViewer.dart
+++ basic_front/lib/Staff/Staff_ProfileViewer.dart
@@ -2,7 +2,12 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:basic_front/BuildPresets/Child_ProfileViewer.dart';
+import 'package:basic_front/NoteAddition.dart';
+import 'package:basic_front/NoteView.dart';
+import 'package:basic_front/REST/Get_RetrieveNotes.dart';
 import 'package:basic_front/Structs/Child.dart';
+import 'package:basic_front/Structs/Note.dart';
+import 'package:basic_front/Structs/Profile.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -13,8 +18,9 @@ import '../Structs/Choice.dart';
 import '../SuspensionView.dart';
 
 class Staff_ProfileViewer_Page extends StatefulWidget {
-  Staff_ProfileViewer_Page({Key key, this.child}) : super(key: key);
+  Staff_ProfileViewer_Page({Key key, this.profile, this.child}) : super(key: key);
 
+  final Profile profile;
   final Child child;
 
   @override
@@ -102,149 +108,148 @@ class Staff_ProfileViewer_State extends State<Staff_ProfileViewer_Page> {
         title: Text(widget.child.firstName +  " " + widget.child.lastName),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            buildPictureNameRow(widget.child.firstName +  " " + widget.child.lastName),
-            buildBirthdayAndGradeRow(),
-            Container(
-              child: IntrinsicHeight(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>
-                    [
-                      Container(
-                        child: FlatButton(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          buildPictureNameRow(widget.child.firstName +  " " + widget.child.lastName),
+          buildBirthdayAndGradeRow(),
+          Container(
+            child: IntrinsicHeight(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>
+                  [
+                    Container(
+                      child: FlatButton(
                           child: Text("Suspend", style: TextStyle(color: Colors.white)),
                           onPressed: ()
-                            {
-                              checkSuspensionResponse();
-                            }
-                        ),
-                        decoration: new BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: new BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
-                          ),
-                        ),
-                        margin: EdgeInsets.only(left: 20),
+                          {
+                            checkSuspensionResponse();
+                          }
                       ),
-                      Flexible(
-                        child: TextField(
-                          textAlign: TextAlign.left,
-                          controller: suspensionController,
-                          decoration: new InputDecoration(
-                            labelText: "Suspension Status",
-                            border: new OutlineInputBorder(
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(20),
-                                bottomRight: Radius.circular(20),
-                              ),
-                              borderSide: new BorderSide(
-                                color: Colors.black,
-                                width: 0.5,
-                              ),
+                      decoration: new BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: new BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                        ),
+                      ),
+                      margin: EdgeInsets.only(left: 20),
+                    ),
+                    Flexible(
+                      child: TextField(
+                        textAlign: TextAlign.left,
+                        controller: suspensionController,
+                        decoration: new InputDecoration(
+                          labelText: "Suspension Status",
+                          border: new OutlineInputBorder(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                            borderSide: new BorderSide(
+                              color: Colors.black,
+                              width: 0.5,
                             ),
                           ),
-                          enabled: false,
-                          style: TextStyle(fontSize: 16, color: Colors.black),
                         ),
+                        enabled: false,
+                        style: TextStyle(fontSize: 16, color: Colors.black),
                       ),
-                    ]
-                ),
+                    ),
+                  ]
               ),
-              margin: EdgeInsets.only(top: 25, left: 25, right: 25),
             ),
-            Container(
-              child: IntrinsicHeight(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>
-                    [
-                      Container(
-                        child: Text(
-                          "Notes",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 28, color: Colors.white),
-                        ),
-                        decoration: new BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: new BorderRadius.only(
-                            topRight: new Radius.circular(20), topLeft: new Radius.circular(20),
-                          ),
-                        ),
-                        padding: EdgeInsets.all(20),
+            margin: EdgeInsets.only(top: 25, left: 25, right: 25),
+          ),
+          Container(
+            child: IntrinsicHeight(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>
+                  [
+                    Container(
+                      child: Text(
+                        "Notes",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 28, color: Colors.white),
                       ),
-                      Container(
-                        child: FlatButton(
-                          child: Text("Add Note", style: TextStyle(color: Colors.white)),
-                          onPressed: () {
-                            setState(() {
-                              isAddingNote = isAddingNote ?  false :  true;
-                            });
+                      decoration: new BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: new BorderRadius.only(
+                          topRight: new Radius.circular(20), topLeft: new Radius.circular(20),
+                        ),
+                      ),
+                      padding: EdgeInsets.all(20),
+                    ),
+                    Container(
+                      child: FlatButton(
+                        child: Text("Add Note", style: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => NoteAdditionPage(profile: widget.profile, child: widget.child)));
+                        },
+                      ),
+                      decoration: new BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: new BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      margin: EdgeInsets.only(left: 20),
+                    ),
+                  ]
+              ),
+            ),
+            margin: EdgeInsets.only(top: 10, left: 10),
+          ),
+          FutureBuilder(
+              future: storage.readToken().then((value) {
+                return RetrieveNotes(value, widget.child.id);
+              }),
+              builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return new Text('Issue Posting Data');
+                  case ConnectionState.waiting:
+                    return new Center(child: new CircularProgressIndicator());
+                  case ConnectionState.active:
+                    return new Text('');
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Text("Press 'Scan QR' to begin!");
+                    } else {
+                      return Expanded(
+                        child: new ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              child: ListTile(
+                                title: Text('${snapshot.data[index].content}',
+                                    style: TextStyle(color: Colors.white)),
+                                dense: false,
+                                onTap: ()
+                                {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => NoteViewPage(note: snapshot.data[index])));
+                                },
+                              ),
+                              color: Colors.blue[colorCodes[index%2]],
+
+                              margin: EdgeInsets.only(left: 10, right: 10),
+                            );
                           },
                         ),
-                        decoration: new BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: new BorderRadius.all(
-                            Radius.circular(20),
-                          ),
-                        ),
-                        margin: EdgeInsets.only(left: 20),
-                      ),
-                    ]
-                ),
-              ),
-              margin: EdgeInsets.only(top: 10, left: 10),
-            ),
-            isAddingNote ? Container(
-              child: TextField(
-                textAlign: TextAlign.left,
-                decoration: new InputDecoration(
-                  labelText: "Note Addition",
-                  border: new OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    ),
-                    borderSide: new BorderSide(
-                      color: Colors.black,
-                      width: 0.5,
-                    ),
-                  ),
-                ),
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-            ) : Container(),
-            Expanded(
-              child: Container(
-                child: ListView.builder(
-                  itemCount: notes.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      child: ListTile(
-                        title: Text('${notes[index]}',
-                            style: TextStyle(color: Colors.white)),
-                        trailing: PopupMenuButton<Choice>(
-                          onSelected: _select,
-                          itemBuilder: (BuildContext context) {
-                            return ReturnDummyList();
-                          },
-                        ),
-                        dense: false,
-                      ),
-                      color: Colors.blue[colorCodes[index%2]],
-                    );
-                  },
-                ),
-                margin: EdgeInsets.only(left: 10, bottom: 10, right: 10),
-              ),
-            ),
-          ],
-        ),
+                      );
+                    }
+                    break;
+                  default:
+                    return null;
+                }
+              }
+          ),
+        ],
+      ),
     );
   }
 
