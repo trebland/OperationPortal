c62d4ac4e0c148405966cda6b2cdcaa26dbd1917
Jacqueline van der Meulen <jackienvdm@knights.ucf.edu>
2020-04-10 16:54:44 -0400

    Added SuspensionModel

---
+++ API/Models/SuspensionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace API.Models
+{
+    public class SuspensionModel
+    {
+        public SuspensionModel(DateTime start, DateTime end)
+        {
+            SuspendedStart = start;
+            SuspendedEnd = end;
+        }
+
+        public DateTime SuspendedStart { get; set; }
+        public DateTime SuspendedEnd { get; set; }
+    }
+}
