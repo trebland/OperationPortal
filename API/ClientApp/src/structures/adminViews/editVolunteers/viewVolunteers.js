5e13f19d7346876910a80441dd89c16a9a361015
baldwinsuen <suenbaldwin@gmail.com>
2020-04-16 14:05:43 -0400

    merge conflicts

--- API/ClientApp/src/structures/adminViews/editVolunteers/viewVolunteers.js
+++ API/ClientApp/src/structures/adminViews/editVolunteers/viewVolunteers.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Card } from 'react-bootstrap/'
+import { Button, Card, Form, FormControl } from 'react-bootstrap/'
 import { Redirect } from 'react-router-dom'
 import './cards.css'
 
